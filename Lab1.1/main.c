293e44fe7f2aadb82b968f7153410f8721b52bd7
RatatornAmornwatee <ratatorn.am@gmail.com>
2025-11-24 21:32:00 +0700

    Add Lab1.1 , Lab1.2 , Lab1.3 , README.md

---
+++ Lab1.1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int N , i ;
+
+  if( scanf( "%d" , &N ) != 1 ) {
+    return 1 ;
+  }//end if
+
+  for( i = 0 ; i < N ; i++ ) {
+    printf( "Hello Loop!\n" ) ;
+  }//end for
+
+  return 0 ;
+}//end main function
