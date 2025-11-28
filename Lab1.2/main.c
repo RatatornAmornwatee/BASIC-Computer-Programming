8125c0c49511775bc32bd2361024878e7340bce2
RatatornAmornwatee <ratatorn.am@gmail.com>
2025-11-28 15:31:44 +0700

    Fix Lap1.1

--- Lab1.2/main.c
+++ Lab1.2/main.c
@@ -3,20 +3,23 @@
 int main() {
   int score ;
 
-  if( scanf( "%d" , &score ) != 1 ) {
+  // Get score input and varidate
+  if( scanf ( "%d" , &score ) != 1 ) {
+    printf (" Error" ) ;
     return 1 ;
   }//end if
 
-  if( score >= 80 ) 
-    printf( "Grade A" ) ;
-  else if( score >= 70 && score < 80 )
-    printf( "Grade B" ) ;
-  else if( score >= 60 && score < 70 )
-    printf( "Grade C" ) ;
-  else if( score >= 50 && score < 60 )
-    printf( "Grade D" ) ;
+  // Grading from score
+  if ( score >= 80 ) 
+    printf ( "Grade A" ) ;
+  else if ( score >= 70 && score < 80 )
+    printf ( "Grade B" ) ;
+  else if ( score >= 60 && score < 70 )
+    printf ( "Grade C" ) ;
+  else if ( score >= 50 && score < 60 )
+    printf ( "Grade D" ) ;
   else
-    printf( "Grade F" ) ;
+    printf ( "Grade F" ) ;
 
   return 0 ;
 }//end main function
