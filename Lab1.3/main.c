a833cef853c34ae254d4f5f744969c2d388395db
RatatornAmornwatee <ratatorn.am@gmail.com>
2025-11-29 17:24:40 +0700

    Major fix

--- Lab1.3/main.c
+++ Lab1.3/main.c
@@ -3,29 +3,30 @@
 int main() {
   int num1 , num2 , operationCode , result ;
 
-  if( scanf( "%d %d %d" , &num1 , &num2 , &operationCode ) != 3 ) {
+  if ( scanf ( "%d %d %d" , &num1 , &num2 , &operationCode ) != 3 ) {
+    printf ( "Error" ) ;
     return 1 ;
   }
 
-  switch( operationCode ) {
-    case 1 :
+  switch ( operationCode ) {
+    case 1 : 
       result = num1 + num2 ;
-      break ;
-    case 2 :
+      break;
+    case 2 : 
       result = num1 - num2 ;
-      break ;
-    case 3 :
+      break;
+    case 3 : 
       result = num1 * num2 ;
-      break ;
-    case 4 :
+      break;
+    case 4 : 
       result = num1 / num2 ;
-      break ;
+      break;
     default :
       printf( "Invalid Operation" ) ;
       return 0 ;
   }
 
-  printf( "%d" , result ) ;
+  printf ( "%d" , result ) ;
 
   return 0 ;
 }
