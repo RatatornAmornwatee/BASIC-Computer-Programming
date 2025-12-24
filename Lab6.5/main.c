4d9e185d85c20890cccee13c96dff6b0b2cb1a54
RatatornAmornwatee <ratatorn.am@gmail.com>
2025-12-24 16:09:38 +0700

    Fix Lab6.5

--- Lab6.5/main.c
+++ Lab6.5/main.c
@@ -2,33 +2,33 @@
 
 #define MAX_SIZE 50
 
-int count_vowels_with_pointer(char *str);
+int count_vowels_with_pointer( char *str ) ;
 
 int main() {
-    char sentence[MAX_SIZE];
-    int vowel_count;
+    char sentence[ MAX_SIZE ] ;
+    int vowel_count ;
 
-    printf("Enter a sentence: ");
+    printf( "Enter a sentence: " ) ;
 
-    if (fgets(sentence, MAX_SIZE, stdin) == NULL) {
-        return 1;
+    if ( fgets( sentence , MAX_SIZE , stdin ) == NULL) {
+        return 1 ;
     }// end if
 
-    sentence[strcspn(sentence, "\n")] = '\0';
+    sentence[ strcspn( sentence , "\n" ) ] = '\0' ;
 
-    vowel_count = 0;
-    char *char_ptr = sentence;
+    vowel_count = 0 ;
+    char *char_ptr = sentence ;
 
     vowel_count = count_vowels_with_pointer( char_ptr ) ;
 
-    printf("\n--- STRING ANALYSIS REPORT ---\n");
-    printf("Input String: \"%s\"\n", sentence);
-    printf("Total Vowel Count: %d\n", vowel_count);
+    printf( "\n--- STRING ANALYSIS REPORT ---\n" ) ;
+    printf( "Input String: \"%s\"\n" , sentence ) ;
+    printf( "Total Vowel Count: %d\n" , vowel_count ) ;
 
-    return 0;
+    return 0 ;
 }// end main function
 
-int count_vowels_with_pointer(char *str) {
+int count_vowels_with_pointer( char *str ) {
     int counter = 0 ;
     int i = 0 ;
     
