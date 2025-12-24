1cb5f366537fbd97a0d44727a64f176688898029
RatatornAmornwatee <ratatorn.am@gmail.com>
2025-12-24 16:05:30 +0700

    Add Lab6.5

---
+++ Lab6.5/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 50
+
+int count_vowels_with_pointer(char *str);
+
+int main() {
+    char sentence[MAX_SIZE];
+    int vowel_count;
+
+    printf("Enter a sentence: ");
+
+    if (fgets(sentence, MAX_SIZE, stdin) == NULL) {
+        return 1;
+    }// end if
+
+    sentence[strcspn(sentence, "\n")] = '\0';
+
+    vowel_count = 0;
+    char *char_ptr = sentence;
+
+    vowel_count = count_vowels_with_pointer( char_ptr ) ;
+
+    printf("\n--- STRING ANALYSIS REPORT ---\n");
+    printf("Input String: \"%s\"\n", sentence);
+    printf("Total Vowel Count: %d\n", vowel_count);
+
+    return 0;
+}// end main function
+
+int count_vowels_with_pointer(char *str) {
+    int counter = 0 ;
+    int i = 0 ;
+    
+    while ( *(str + i) != '\0' ) {
+        if ( *(str+i) == 'a' || *(str+i) == 'e' || *(str+i) == 'i' || *(str+i) == 'o' || *(str+i) == 'u' ) {
+            counter++ ;
+        }
+        else if ( *(str+i) == 'A' || *(str+i) == 'E' || *(str+i) == 'I' || *(str+i) == 'O' || *(str+i) == 'U' ) {
+            counter++ ;
+        }// end if else
+        i++ ;
+    }// end while loop
+
+    return counter ;
+}// end count_vowels_with_pointer function
